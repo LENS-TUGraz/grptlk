cd28b0a97dc271433433a57e675b8635ce248f38
GatCode <gatcode@gmail.com>
2025-09-17 10:33:35 +0200

    Add grptlk recv datapaths

--- samples/iso_receive/src/main.c
+++ samples/iso_receive/src/main.c
@@ -342,13 +342,22 @@ int main(void)
 				.format = BT_HCI_CODING_FORMAT_TRANSPARENT,
 			};
 
+			/* Setup all BISes for RX */
 			err = bt_iso_setup_data_path(&bis_iso_chan[chan], BT_HCI_DATAPATH_DIR_CTLR_TO_HOST, &hci_path);
 			if (err != 0)
 			{
-				printk("Failed to setup ISO TX data path: %d\n", err);
+				printk("Failed to setup ISO RX data path: %d\n", err);
 			}
 
-			// TODO: use BT_HCI_DATAPATH_DIR_HOST_TO_CTLR for all BISes > 0
+			/* Setup all BISes != BIS0 also for TX */
+			if (chan > 0)
+			{
+				err = bt_iso_setup_data_path(&bis_iso_chan[chan], BT_HCI_DATAPATH_DIR_HOST_TO_CTLR, &hci_path);
+				if (err != 0)
+				{
+					printk("Failed to setup ISO TX data path: %d\n", err);
+				}
+			}
 
 			printk("Setting data path complete chan %u.\n", chan);
 		}
